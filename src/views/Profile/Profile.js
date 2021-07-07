1a075f1e4b0ca9735c4a41b6a16996baeb6336d7
Tsabit Abdul Aziz <tsabit830@gmail.com>
2021-07-07 10:32:19 +0700

    displaying info profile

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -4,10 +4,11 @@ import {
     Card, 
     CardHeader, 
     Container, 
-    CardContent,
     CardActions,
     Divider,
-    Button
+    Button,
+    ListItemText,
+    IconButton
 } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
@@ -16,14 +17,24 @@ import { setLoggedOut } from '../../actions/auth';
 import PropTypes from 'prop-types'
 import { ConfirmModal } from '../../components'
 import { DialogContentText } from '@material-ui/core';
+import { List } from '@material-ui/core';
+import { ListItem } from '@material-ui/core';
+import { Icon } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     root: {
         marginTop: theme.spacing(2)
+    },
+    card: {
+        margin: theme.spacing(2)
+    },
+    grid: {
+        height: '80vh'
     }
 }))
 
 const Profile = props => {
+    const { session } = props;
     const [open, setOpen] = useState(false);
     const classes = useStyles();
     const transRef = useSpringRef();
@@ -51,18 +62,50 @@ const Profile = props => {
                 <ConfirmModal open={open} handleClose={() => setOpen(false)} onAggre={handleLogout}>
                     <DialogContentText style={{textAlign: 'center'}}>Apakah anda yakin untuk keluar dari aplikasi?</DialogContentText>
                 </ConfirmModal>
-                <Grid container spacing={4}>
-                    <Grid item xs={12}>
-                        <Card>
+                <Grid container spacing={4} justify='center' alignItems='center' className={classes.grid}>
+                    <Grid item xs={12} sm={8} md={5}>
+                        <Card className={classes.card}>
                             <CardHeader 
-                                title='Profile'
+                                title='Profile' 
+                                subheader={session.email} 
+                                action={<IconButton>
+                                    <Icon>edit</Icon>
+                                </IconButton>}
                             />
                             <Divider />
-                            <CardContent>
-                                <p>content here!</p>
-                            </CardContent>
-                            <CardActions style={{justifyContent: 'center'}}>
-                                <Button variant='contained' onClick={() => setOpen(true)}>Logout</Button>
+                            <List>
+                                <ListItem dense>
+                                    <ListItemText 
+                                        primary='Nama' 
+                                        secondary={session.fullname}
+                                    />                                    
+                                </ListItem>
+                                <ListItem dense>
+                                    <ListItemText 
+                                        primary='Phone' 
+                                        secondary={session.phone}
+                                    />
+                                </ListItem>
+                                <ListItem dense>
+                                    <ListItemText 
+                                        primary='Pos Office' 
+                                        secondary={`KPRK - ${session.office}`}
+                                    />
+                                </ListItem>
+                                { Object.keys(session.details).length > 0 && <ListItem dense>
+                                    <ListItemText 
+                                        primary='Office' 
+                                        secondary={<React.Fragment>
+                                            {`${session.details.city}, ${session.details.kecamatan}, ${session.details.kelurahan} (${session.details.kodepos})`}
+                                            <br/>
+                                            { session.details.address}
+                                        </React.Fragment>}
+                                    />
+                                </ListItem>}
+                            </List>
+                            <CardActions style={{justifyContent: 'flex-end'}}>
+                                <Button variant='outlined' onClick={() => setOpen(true)}>Ganti Password</Button>
+                                <Button variant='outlined' onClick={() => setOpen(true)}>Logout</Button>
                             </CardActions>
                         </Card>
                     </Grid>
@@ -74,6 +117,13 @@ const Profile = props => {
 
 Profile.propTypes = {
     setLoggedOut: PropTypes.func.isRequired,
+    session: PropTypes.object.isRequired,
 }
 
-export default connect(null, { setLoggedOut })(Profile);
+function mapStateToProps(state) {
+    return{
+        session: state.auth
+    }
+}
+
+export default connect(mapStateToProps, { setLoggedOut })(Profile);
