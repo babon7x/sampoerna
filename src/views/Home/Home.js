36fca44e629651ce0634555670158014554608aa
Tsabit Abdul Aziz <tsabit830@gmail.com>
2021-07-12 19:33:17 +0700

    add transition

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -5,14 +5,14 @@ import {
     Grow, 
     makeStyles,
 } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
-import { Alert } from '@material-ui/lab';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
 import { ResultOrder, PoUsed } from './components';
 import { getResultOrder, getPoUsed } from '../../actions/dashboard';
 import { setLoadingProgress } from '../../actions/loadingprogress';
 import { setMessage } from '../../actions/notification';
+import { animated, useSpringRef, useTransition } from 'react-spring';
 
 
 const useStyles = makeStyles(theme => ({
@@ -32,23 +32,23 @@ const Home = props => {
     const classes = useStyles();
     const { user } = props;
 
-    const [open, setOpen] = useState(true);
+    const transRef = useSpringRef();
+    const transitions = useTransition(null, {
+        ref: transRef,
+        keys: null,
+        from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+        enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+        leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+    })
 
     useEffect(() => {
-        if(open){
-            setTimeout(() => {
-                setOpen(false);
-            }, 3000);
-        }
-    }, [open]);
-
-    useEffect(() => {
+        transRef.start();
         (async () => {
-            props.setLoadingProgress(10);
+            props.setLoadingProgress(20);
             const { token, userid, details } = user;
             try {
                 await props.getResultOrder({ token, userid });
-                props.setLoadingProgress(20);
+                props.setLoadingProgress(50);
 
                 await props.getPoUsed({ token, userid, officeid: details.officeid });
             } catch (error) {
@@ -59,35 +59,27 @@ const Home = props => {
         //eslint-disable-next-line
     }, []);
     
-    return(
-        <div className={classes.root}>
-            <Grow in={open} timeout={500}>
-                <Alert severity='success' variant='filled' className={classes.alert}>
-                    Selamat datang kembali { user.fullname }
-                </Alert>
-            </Grow>
-
-            <Container>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm={12} md={9}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                                <ResultOrder list={props.orders} />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Card style={{height: '68.2vh'}} raised>
-                                    <p>grph</p>
-                                </Card>
-                            </Grid>
+    return transitions(style => <animated.div style={{ ...style }} className={classes.root}>
+        <Container>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={12} md={9}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <ResultOrder list={props.orders} />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Card style={{height: '68.2vh'}} raised>
+                                <p>grph</p>
+                            </Card>
                         </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={12} md={3}>
-                        <PoUsed list={props.purchases} onClick={() => props.history.push("/purchaselist")} />
-                    </Grid>
                 </Grid>
-            </Container>
-        </div>
-    )
+                <Grid item xs={12} sm={12} md={3}>
+                    <PoUsed list={props.purchases} onClick={() => props.history.push("/purchaselist")} />
+                </Grid>
+            </Grid>
+        </Container>
+    </animated.div>)
 }
 
 Home.propTypes = {
