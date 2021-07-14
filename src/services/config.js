b03082ad3c8261ef400120f17888c9851af160e1
Tsabit Abdul Aziz <tsabit830@gmail.com>
2021-07-14 15:27:19 +0700

    Add platform api

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,20 @@
-export const url = 'http://localhost/sampoerna'
-export const headers = {
-    'Content-Type': 'application/json',
-    //'Accept': 'application/json',
-    'X-POS-KEY': 'ebc15ef93d40c9386b3713b0ce3e4349'
+import { platform } from './platform';
+let urldefault = '';
+const headersdefault = {};
+
+if(platform === 'localhost'){
+    urldefault = "http://localhost/sampoerna";
+    headersdefault['Content-Type'] = 'application/json';
+    headersdefault['X-POS-KEY'] = 'ebc15ef93d40c9386b3713b0ce3e4349';
+}else if(platform === 'dev'){
+    urldefault = "http://10.60.64.76:9445/sampoerna";
+    headersdefault['Content-Type'] = 'application/json';
+    headersdefault['X-POS-KEY'] = 'ebc15ef93d40c9386b3713b0ce3e4349';
+}else{
+    urldefault = "https://order.posindonesia.co.id";
+    headersdefault['Content-Type'] = 'application/json';
+    headersdefault['X-POS-KEY'] = 'ebc15ef93d40c9386b3713b0ce3e4349';
 }
+
+export const url = urldefault;
+export const headers = headersdefault;
